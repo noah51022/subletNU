12ac310dfb05425197fa47f444cb1e2d5d6a5631
Noah Torres <43016877+noah51022@users.noreply.github.com>
2025-04-18 12:09:26 -0400

    added vercel/analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "@/contexts/AppContext";
+import { Analytics } from '@vercel/analytics/react';
 
 
 // Pages
@@ -28,6 +29,7 @@ const App = () => {
           <TooltipProvider>
             <Toaster />
             <Sonner />
+            <Analytics />
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/create" element={<CreateSubletPage />} />
